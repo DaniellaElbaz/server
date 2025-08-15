c84d8d3b3c7ee9ef2e6058b26767eb2ea0e40909
Daniella <daniellaelbaz711@gmail.com>
2025-08-16 01:45:56 +0300

    get parents or children from table

--- routers/familyRouter.js
+++ routers/familyRouter.js
@@ -6,5 +6,7 @@ router.post('/register', familyController.registerFamily);
 router.post('/registerParentsChildren', familyController.registerParentsChildren);
 router.post('/login', familyController.login);
 router.post('/registerMembers', familyController.registerMembers);
+router.get('/children', familyController.listChildren);
+router.get('/parents',  familyController.listParents);
 
 module.exports = router;
