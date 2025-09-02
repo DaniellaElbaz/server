94700538125930fa7b889cea9823bae01332e057
Daniella <daniellaelbaz711@gmail.com>
2025-09-02 17:40:52 +0300

    fix eroor

--- index.js
+++ index.js
@@ -43,9 +43,10 @@ app.get('/parent-calendar.html', (_req, res) =>
   res.sendFile(path.join(CLIENT_DIR, 'parent-calendar.html'))
 );
 
-app.get('*', (_req, res) => {
+app.get('/*', (_req, res) => {
   res.sendFile(path.join(CLIENT_DIR, 'index.html'));
 });
+
 
 /** Start server */
 const PORT = process.env.PORT || 3000;
